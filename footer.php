b289dfcddcc6e343056712af124b122ea865d9ab
sandesh9988 <sandesh@lastdoorsolutions.com>
2022-10-17 22:29:32 +0545

    added: footer with fully responsive

--- footer.php
+++ footer.php
@@ -1,15 +1,99 @@
 <footer class="footer">
     <div class="container">
-        <div class="footer-left">
+        <div class="footer-content">
+            <div class="footer-left">
+                <div class="footer-left__top">
+                    <h3 class="footer__heading">Links</h3>
+                    <div class="footer-left__links">
+                        <ul class="footer__lists">
+                            <li><a href="#">Links</a></li>
+                            <li><a href="#">Tenders</a></li>
+                            <li><a href="#">Bills</a></li>
+                            <li><a href="#">Trainings</a></li>
+                            <li><a href="#">Staffs</a></li>
+                            <li><a href="#">Photo Gallery</a></li>
+                        </ul>
+                        <ul class="footer__lists">
+                            <li><a href="#">Sitemap</a></li>
+                            <li><a href="#">Citizen Charter</a></li>
+                            <li><a href="#">Objectives</a></li>
+                            <li><a href="#">Resources</a></li>
+                            <li><a href="#">E Consultations</a></li>
+                            <li><a href="#">Online Services</a></li>
+                        </ul>
+                        <ul class="footer__lists">
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Major Programs</a></li>
+                        </ul>
 
-        </div>
-        <div class="footer-center">
+                    </div>
 
+                </div>
+
+            </div>
+            <div class="footer-center">
+                <h3 class="footer__heading">Office Hours (Summer)</h3>
+                <p>Sunday - Thursday: 10:00 A.M. - 5:00 P.M. <br>Friday: 10:00 A.M. - 3:30 P.M.</p><br>
+                <h3 class="footer__heading">Office Hours (Winter)</h3>
+                <p>Sunday - Thursday: 10:00 A.M. - 4:00 P.M. <br>Friday: 10:00 A.M. - 3:00 P.M.</p>
+            </div>
+            <div class="footer-right">
+                <div class="footer-right__info">
+                    <div class="footer-right__header">
+                        <svg class="icon icon-locations">
+                            <use xlink:href="#icon-locations"></use>
+                        </svg>
+                        <h3 class="footer__heading">Location</h3>
+                    </div>
+                    <p>Thapagaun, New Baneshwor,<br> Kathmandu Nepal</p>
+                </div>
+                <div class="footer-right__info">
+                    <div class="footer-right__header">
+                        <svg class="icon icon-email">
+                            <use xlink:href="#icon-email"></use>
+                        </svg>
+                        <h3 class="footer__heading">Email</h3>
+                    </div>
+                    <a class="footer__info-link" href="info@doit .gov.np">info@doit .gov.np</a>
+                </div>
+                <div class="footer-right__info">
+                    <div class="footer-right__header">
+                        <svg class="icon icon-phone">
+                            <use xlink:href="#icon-phone"></use>
+                        </svg>
+                        <h3 class="footer__heading">Call us</h3>
+                    </div>
+                    <a class="footer__info-link" href="tel:+977-1-5244539">+977-1-5244539</a>, <a class="footer__info-link" href="tel:+977-1-5244167">+977-1-5244167</a>,<br><a class="footer__info-link" href="tel:+977-1-5244705">+977-1-5244705</a>
+                </div>
+                <div class="footer-right__info">
+                    <div class="footer-right__header">
+                        <svg class="icon icon-fax">
+                            <use xlink:href="#icon-fax"></use>
+                        </svg>
+                        <h3 class="footer__heading">Fax</h3>
+                    </div>
+                    <a class="footer__info-link" href="tel:+977 1 5244598">+977 1 5244598</a>
+                </div>
+                <div class="footer-right__info">
+                    <div class="footer-right__header">
+                        <svg class="icon icon-audio">
+                            <use xlink:href="#icon-audio"></use>
+                        </svg>
+                        <h3 class="footer__heading">Audio Notice Board</h3>
+                    </div>
+                    <p>1618014493198</p>
+                </div>
+
+            </div>
         </div>
-        <div class="footer-right">
-            
+        <div class="footer-left__bottom">
+            <p>© Copyright 2021 .All Rights Reserved.</p><br>
+            <p>Government of Nepal<br>Ministry of Communication and Information Technology<br>Department of Information Technology.</p>
+
         </div>
     </div>
+
 </footer>
 <?php wp_footer(); ?>
 <svg style="position:absolute;width:0;height:0" overflow="hidden">
@@ -24,8 +108,26 @@
             <path d="M32 6.231c-1.177 0.515-2.443 0.861-3.771 1.017 1.356-0.799 2.397-2.066 2.887-3.575-1.268 0.74-2.673 1.278-4.169 1.568-1.196-1.256-2.904-2.041-4.792-2.041-4.239 0-7.353 3.893-6.396 7.934-5.455-0.269-10.292-2.842-13.531-6.752-1.72 2.905-0.892 6.705 2.031 8.629-1.075-0.034-2.088-0.324-2.972-0.809-0.072 2.994 2.108 5.795 5.265 6.418-0.924 0.247-1.936 0.305-2.965 0.11 0.835 2.567 3.259 4.435 6.133 4.488-2.76 2.13-6.237 3.082-9.72 2.678 2.905 1.834 6.357 2.903 10.064 2.903 12.189 0 19.076-10.134 18.66-19.224 1.283-0.912 2.396-2.050 3.276-3.346z"></path>
         </symbol>
         <symbol id="icon-arrow" viewBox="0 0 56 32">
-            <path  d="M8.5 0.28l-6 6 25.456 25.456 25.456-25.456-6-6-19.456 19.456-19.456-19.456z"></path>
+            <path d="M8.5 0.28l-6 6 25.456 25.456 25.456-25.456-6-6-19.456 19.456-19.456-19.456z"></path>
         </symbol>
+        <symbol id="icon-locations" viewBox="0 0 23 32">
+            <path d="M0 11.522c0-6.363 5.117-11.522 11.429-11.522s11.429 5.158 11.429 11.522c0 6.528-7.19 16.163-10.165 19.868-0.653 0.813-1.874 0.813-2.527 0-2.975-3.705-10.165-13.341-10.165-19.868zM7.347 11.522c0 2.273 1.827 4.115 4.082 4.115s4.082-1.842 4.082-4.115c0-2.273-1.827-4.115-4.082-4.115s-4.082 1.842-4.082 4.115z"></path>
+        </symbol>
+        <symbol id="icon-email" viewBox="0 0 40 32">
+            <path d="M0.047 3.531c0.234-1.986 1.905-3.531 3.953-3.531h32c2.048 0 3.719 1.545 3.953 3.531l-19.953 12.469-19.953-12.469zM0 28v-20.496l18.941 11.836c0.648 0.404 1.469 0.404 2.117 0l18.941-11.836v20.496c0 2.21-1.79 4-4 4h-32c-2.21 0-4-1.79-4-4z"></path>
+        </symbol>
+        <symbol id="icon-phone" viewBox="0 0 32 32">
+            <path d="M28.847 21.804l-4.489-0.517c-1.087-0.125-2.155 0.249-2.921 1.015l-3.296 3.296c-2.528-1.283-4.82-2.955-6.801-4.938-1.983-1.981-3.655-4.273-4.938-6.801l3.296-3.296c0.766-0.766 1.14-1.835 1.015-2.921l-0.517-4.489c-0.214-1.799-1.728-3.153-3.545-3.153h-3.082c-2.013 0-3.687 1.675-3.563 3.687 0.472 7.605 3.748 14.443 8.805 19.501s11.896 8.333 19.501 8.805c2.013 0.125 3.688-1.55 3.688-3.563v-3.082c0-1.817-1.354-3.331-3.153-3.545z"></path>
+        </symbol>
+        <symbol id="icon-fax" viewBox="0 0 34 32">
+            <path d="M15.508 1.684c0-0.93 0.771-1.684 1.723-1.684h10.338c0.952 0 1.723 0.754 1.723 1.684v5.053h1.723c1.901 0 3.446 1.511 3.446 3.368v11.79c0 1.858-1.546 3.369-3.446 3.369h-12.062v1.684c0 2.77-2.335 5.053-5.169 5.053h-6.892c-2.834 0-5.169-2.282-5.169-5.053v-1.839c-1.002-0.348-1.723-1.274-1.723-2.372v-16.842c0-1.395 1.158-2.526 2.585-2.526h1.723c1.427 0 2.585 1.132 2.585 2.526v16.842c0 1.098-0.722 2.024-1.723 2.372v1.839c0 0.952 0.75 1.684 1.723 1.684h6.892c0.974 0 1.723-0.733 1.723-1.684v-1.684h-1.723c-1.901 0-3.446-1.511-3.446-3.369v-11.79c0-1.858 1.546-3.368 3.446-3.368h1.723v-5.053zM25.846 3.368h-6.892v5.053h6.892v-5.053zM19.815 11.789c0.476 0 0.862 0.377 0.862 0.842v1.684c0 0.465-0.386 0.842-0.862 0.842h-1.723c-0.476 0-0.862-0.377-0.862-0.842v-1.684c0-0.465 0.386-0.842 0.862-0.842h1.723zM27.569 12.632c0-0.465-0.386-0.842-0.862-0.842h-1.723c-0.476 0-0.862 0.377-0.862 0.842v1.684c0 0.465 0.386 0.842 0.862 0.842h1.723c0.476 0 0.862-0.377 0.862-0.842v-1.684zM19.815 18.526c0.476 0 0.862 0.377 0.862 0.842v1.684c0 0.465-0.386 0.842-0.862 0.842h-1.723c-0.476 0-0.862-0.377-0.862-0.842v-1.684c0-0.465 0.386-0.842 0.862-0.842h1.723zM27.569 19.368c0-0.465-0.386-0.842-0.862-0.842h-1.723c-0.476 0-0.862 0.377-0.862 0.842v1.684c0 0.465 0.386 0.842 0.862 0.842h1.723c0.476 0 0.862-0.377 0.862-0.842v-1.684z"></path>
+        </symbol>
+        <symbol id="icon-audio" viewBox="0 0 34 32">
+            <path d="M28.4 1.215c0.381-0.743 1.156-1.215 2.005-1.215 1.239 0 2.242 0.984 2.242 2.198v10.726c1.121 0.635 1.813 1.807 1.814 3.076-0.002 1.268-0.693 2.439-1.814 3.073v10.729c0 1.214-1.004 2.198-2.242 2.198-0.849 0-1.624-0.472-2.005-1.215l-1.194-2.34-11.598-4.872c-0.667-0.279-1.098-0.924-1.098-1.635v-11.878c0-0.711 0.431-1.351 1.098-1.632l11.598-4.872 1.194-2.34zM0 12.444c0-1.956 1.632-3.556 3.628-3.556h5.441c1.001 0 1.814 0.796 1.814 1.778v19.556c0 0.981-0.813 1.778-1.814 1.778s-1.814-0.797-1.814-1.778v-7.111h-3.628c-1.995 0-3.628-1.6-3.628-3.556v-7.111z"></path>
+        </symbol>
+
+
+
     </defs>
 </svg>
 </body>
